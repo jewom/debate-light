1ac004e51f80b55a942447f0a0248cc63f2e9cb1
jerome <j.clamentsanz@gmail.com>
2014-01-05 16:45:22 +0100

    about et images + icons xml

---
+++ Debate/src/com/teamgeny/debate/fragments/FragmentAbout.java
@@ -0,0 +1,37 @@
+package com.teamgeny.debate.fragments;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.liltof.library.tools.PushScale;
+import com.teamgeny.debate.R;
+
+public class FragmentAbout extends FragmentParent {
+	View me;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		me = inflater.inflate(R.layout.fragment_nouveau_projet, null);
+	
+		return me;
+	}
+
+}
